b930d33693b002b0004381132a9785d9a95cf346
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-19 13:43:59 +0300

    added order migration

--- backend/database/migrations/2024_08_19_103633_create_orders_table.php
+++ backend/database/migrations/2024_08_19_103633_create_orders_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
                 $table->text('city');
                 $table->text('comment');
                 $table->timestamps();
+
             });
         }
     }

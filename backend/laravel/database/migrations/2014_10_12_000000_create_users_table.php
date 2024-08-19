ba6987b5343613bc01aee23efe4a45a15cd94162
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-19 13:09:25 +0300

    finalized user table

--- backend/laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (!Schema::hasTable('users')) {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
+    }
     }
 
     /**

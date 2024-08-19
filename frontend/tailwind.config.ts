e267351889de4cbff1a3c44db1914ead9ac44e7a
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-19 12:17:51 +0300

    added thems and plugins

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,15 +6,30 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    require("tailwind-scrollbar"),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F9F7F7",
+          secondary: "#383838",
+          accent: "#B2F042",
+          neutral: "#C9CCD1",
+          "base-100": "#F9F5F6",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+          "base-200": "#D83776",
+          "base-300": "#3B5998",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
 };
+
 export default config;

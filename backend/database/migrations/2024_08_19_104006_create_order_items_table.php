e4beebe53aca29984956413865a57db25589e749
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-19 13:44:11 +0300

    added order item migration

--- backend/database/migrations/2024_08_19_104006_create_order_items_table.php
+++ backend/database/migrations/2024_08_19_104006_create_order_items_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
                 $table->integer('quantity');
                 $table->decimal('total_price', 10, 2);
                 $table->timestamps();
+
             });
         }
     }

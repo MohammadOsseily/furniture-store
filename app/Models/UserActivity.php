523bfb576092e7e213e61986d402a7148a5e396e
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-22 23:34:34 +0300

    added user acitvity and product relation

--- app/Models/UserActivity.php
+++ app/Models/UserActivity.php
@@ -17,5 +17,8 @@ class UserActivity extends Model
         return $this->belongsTo(User::class);
     }
 
-
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }

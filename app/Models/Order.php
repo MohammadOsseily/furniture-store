d38cb6d25c93fdd7cc0f11131f953b147cccdde1
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-22 23:23:27 +0300

    added order model relation

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,11 +1,25 @@
 <?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\OrderItem;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'total_amount', 'status', 'addres_line', 'city', 'comment'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }

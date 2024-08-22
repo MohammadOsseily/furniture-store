86cad35cb57b7026a72ff1e570d1b712d9c5e5b3
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-22 23:53:25 +0300

    added product3d model relations

--- app/Models/Product3d.php
+++ app/Models/Product3d.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product3d extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['product_id', 'model_file_path', 'file_type'];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }

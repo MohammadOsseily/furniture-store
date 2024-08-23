a67f1d5e477e6ea8bd49cbe215eece9497289cfc
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 20:03:37 +0300

    added auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Authentication Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']); // User Registration
+    Route::post('/login', [AuthController::class, 'login']);       // User Login
+    Route::post('/logout', [AuthController::class, 'logout']);     // User Logout
+    Route::post('/refresh', [AuthController::class, 'refresh']);   // Refresh Token
 });
+

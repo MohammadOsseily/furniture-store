f33aeebe5b748d5f802ba21e23db132f1b80dab9
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 19:55:03 +0300

    added category index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class CategoryController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
+
+    // List All Categories
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json([
+            'status' => 'success',
+            'categories' => $categories,
+        ]);
+    }
+
+    // Show Single Category
+
 }

ca4e77f9c2e7d06d6dea20500ad4d176b2beb407
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 19:56:40 +0300

    added update category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -66,5 +66,38 @@ class CategoryController extends Controller
         ], 201);
     }
 
+    // Update Existing Category (Admin Only)
+    public function update(Request $request, $id)
+    {
+        if (Gate::denies('isAdmin')) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized access',
+            ], 403);
+        }
+
+        $category = Category::find($id);
+
+        if(!$category){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Category not found',
+            ], 404);
+        }
+
+        $request->validate([
+            'name' => 'sometimes|string|max:255|unique:categories,name,'.$category->id,
+            'description' => 'sometimes|nullable|string',
+        ]);
+
+        $category->update($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Category updated successfully',
+            'category' => $category,
+        ]);
+    }
+
 
 }

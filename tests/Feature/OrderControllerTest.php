79157cdb78f2bd56773611d2d0e9691dcc976eb9
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 22:54:03 +0300

    added create order test

--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature;
 
+
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Order;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class OrderControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testCreateOrder()
+    {
+        $user = User::factory()->create();
+        $token = JWTAuth::fromUser($user);
+        $product = Product::factory()->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->postJson('/api/orders/create', [
+            'address_line' => '123 Test St',
+            'city' => 'Test City',
+            'items' => [
+                [
+                    'product_id' => $product->id,
+                    'quantity' => 2,
+                ],
+            ],
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'status' => 'success',
+                'message' => 'Order placed successfully',
+            ]);
+
+        $this->assertDatabaseHas('orders', [
+            'user_id' => $user->id,
+            'total_amount' => $product->price * 2,
+        ]);
     }
+
+
 }

7cae30bbda1938881fab90b13ff5ee25680e4e5d
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 20:05:06 +0300

    added order routes

--- routes/api.php
+++ routes/api.php
@@ -61,3 +61,16 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/categories/{id}/delete', [CategoryController::class, 'destroy']);     // Delete Category (Admin Only)
 });
 
+/*
+|--------------------------------------------------------------------------
+| Order Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/orders', [OrderController::class, 'index']);                        // List All Orders (Admin Only)
+    Route::get('/orders/{id}', [OrderController::class, 'show']);                    // Show Single Order
+    Route::post('/orders/create', [OrderController::class, 'store']);                // Create New Order
+    Route::post('/orders/{id}/update', [OrderController::class, 'update']);          // Update Order Status (Admin Only)
+    Route::post('/orders/{id}/delete', [OrderController::class, 'destroy']);         // Delete Order (Admin Only)
+});

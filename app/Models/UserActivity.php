ca8641dadcfd333ed555dbca5c9423a1ffc7af73
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 22:16:45 +0300

    added idel

--- app/Models/UserActivity.php
+++ app/Models/UserActivity.php
@@ -6,6 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $product_id
+ * @property string $interaction_type
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Product $product
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity whereInteractionType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UserActivity extends Model
 {
     use HasFactory;

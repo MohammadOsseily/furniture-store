1b85b9ae773cb5af1687966af1a5610bdb8f2c06
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 19:58:15 +0300

    added product show by id

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,5 +24,23 @@ class ProductController extends Controller
         ]);
     }
 
+    // Show Single Product
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if(!$product){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'product' => $product,
+        ]);
+    }
+
 
 }

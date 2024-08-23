2fd4b42bd75f75374314bbb07198a77c80fd22ce
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 19:53:22 +0300

    added user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    // Get Authenticated User Profile
+    public function profile()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+        ]);
+    }
+
+
 }

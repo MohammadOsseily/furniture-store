a0fdc270b2b7be930a0847e913b49b714d2ecd99
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-23 22:17:08 +0300

    added idel

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -7,6 +7,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\CartProduct> $cartProducts
+ * @property-read int|null $cart_products_count
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ShoppingCart whereUserId($value)
+ * @mixin \Eloquent
+ */
 class ShoppingCart extends Model
 {
     use HasFactory;

163512c0d0cca0df8e524f81ba7f94d947943496
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-26 23:46:57 +0300

    added show

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -18,3 +18,21 @@ class OrderItemController extends Controller
         ], 200);
     }
 
+    // Get a single order item
+    public function show($orderId, $id)
+    {
+        $orderItem = OrderItem::where('order_id', $orderId)->where('id', $id)->first();
+
+        if (!$orderItem) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Order item not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'order_item' => $orderItem,
+        ], 200);
+    }
+

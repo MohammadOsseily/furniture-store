ab0eba090e46d679e714d48fb5410777f4a44d0c
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-26 23:46:35 +0300

    added index

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 
 class OrderItemController extends Controller
 {
-    //
-}
+    // Get all order items (for admin or user to view details of their own order)
+    public function index($orderId)
+    {
+        $orderItems = OrderItem::where('order_id', $orderId)->get();
+
+        return response()->json([
+            'status' => 'success',
+            'order_items' => $orderItems,
+        ], 200);
+    }
+

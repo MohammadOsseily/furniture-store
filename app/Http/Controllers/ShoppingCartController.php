2191669502426d928740b6bc48cec205923590ae
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-26 23:49:32 +0300

    added show

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\ShoppingCart;
+use Illuminate\Support\Facades\Auth;
 
 class ShoppingCartController extends Controller
 {
-    //
-}
+    // View the user's shopping cart
+    public function show()
+    {
+        $cart = ShoppingCart::with('cartProducts.product')->where('user_id', Auth::id())->first();
+
+        if (!$cart) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Shopping cart not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'cart' => $cart,
+        ], 200);
+    }
+

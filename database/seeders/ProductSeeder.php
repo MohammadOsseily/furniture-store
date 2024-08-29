a1aa00a4a6f4ab6b6a4e7e0967e2de16207351f2
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 23:25:35 +0300

    added product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -1,17 +1,16 @@
 <?php
 
+// database/seeders/ProductSeeder.php
+
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Product::factory()->count(5)->create();
     }
 }

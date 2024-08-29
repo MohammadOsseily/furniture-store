4480b18d0a4451d74a699eb307e7a6fad486e4e4
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 23:24:15 +0300

    added product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,22 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
- */
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Product::class;
+
     public function definition(): array
     {
+        $category = Category::inRandomOrder()->first();
+
         return [
-            //
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'category_id' => $category->id,  // Assigning a random category
+            'image' => $this->faker->imageUrl(),
+            'color' => $this->faker->safeColorName(),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }

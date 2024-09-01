5d3a363d50bf88c8bcedde51f1bd42f5a03f3ac6
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-01 23:33:55 +0300

    added stock table to product

--- database/migrations/2024_08_19_101740_create_products_table.php
+++ database/migrations/2024_08_19_101740_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
+            $table->integer('stock')->default(1);
             $table->decimal('price', 10, 2);
             $table->unsignedBigInteger('category_id')->nullable();
             $table->string('image')->nullable();

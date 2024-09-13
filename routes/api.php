6f6de07dc79762c9976b6b97b308871719a1b6e6
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-14 00:52:28 +0300

    added Update 3D Product (Admin Only) api

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,7 @@ Route::get('/product3ds/{id}', [Product3DController::class, 'show']);           
 // Admin routes for managing 3D products
 Route::group(['middleware' => ['auth:api', 'isAdmin']], function () {
     Route::post('/product3ds', [Product3DController::class, 'store']);                   // Create 3D Product (Admin Only)
+    Route::put('/product3ds/{id}', [Product3DController::class, 'update']);              // Update 3D Product (Admin Only)
 
 });
 

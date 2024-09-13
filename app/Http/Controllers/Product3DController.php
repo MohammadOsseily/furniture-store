ca0b1e7b53a28ab6a5ce3fb941cb6ffb9effb864
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-14 00:46:15 +0300

    added index function to get all 3d products

--- app/Http/Controllers/Product3DController.php
+++ app/Http/Controllers/Product3DController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Product3D;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class Product3DController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
+
+    // List All 3D Products
+    public function index(Request $request)
+    {
+        $product3ds = Product3D::with('product:id,name')->get();
+        return response()->json($product3ds, 200);
+    }
+
+
+
 }
